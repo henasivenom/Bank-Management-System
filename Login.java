dcb866e5cc423b642b9411ec3e101c964819d753
Mukesh Patel <Amukeshpatel222@gmail.com>
2025-11-10 22:30:18 +0530

    Created Login Page

---
+++ Login.java
@@ -0,0 +1,74 @@
+package bank.management.sys;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.*;
+
+public class Login extends JFrame{
+	
+	Login() {
+		setTitle("ATM");
+		
+		setLayout(null);
+		
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+		Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2);
+		JLabel label = new JLabel(i3);
+		label.setBounds(70, 10, 100, 100);
+		add(label);
+		
+		JLabel text = new JLabel("Welcome to ATM");
+		text.setFont(new Font("Osward", Font.BOLD, 38));
+		text.setBounds(200, 40,400,40);
+		add(text);
+		
+		JLabel cardno = new JLabel("Card No:");
+		cardno.setFont(new Font("Raleway", Font.BOLD, 28));
+		cardno.setBounds(120, 150 ,150,30);
+		add(cardno);
+		
+		JTextField cardTextField = new JTextField();
+		cardTextField.setBounds(300, 150, 250, 30);
+		add(cardTextField);
+		
+		JLabel pin = new JLabel("PIN:");
+		pin.setFont(new Font("Raleway", Font.BOLD, 28));
+		pin.setBounds(120, 220,250,30);
+		add(pin);
+		
+		JTextField pinTextField = new JTextField();
+		pinTextField.setBounds(300, 220, 250, 30);
+		add(pinTextField);
+		
+		JButton login = new JButton("SIGN IN");
+		login.setBounds(300, 280, 100, 30);
+		login.setBackground(Color.black);
+		login.setForeground(Color.white);
+		add(login);
+		
+		JButton clear = new JButton("CLEAR");
+		clear.setBounds(420, 280, 100, 30);
+		clear.setBackground(Color.black);
+		clear.setForeground(Color.white);
+		add(clear);
+		
+		JButton signup = new JButton("SIGN UP");
+		signup.setBounds(300, 320, 220, 30);
+		signup.setBackground(Color.black);
+		signup.setForeground(Color.white);
+		add(signup);
+		
+		getContentPane().setBackground(Color.WHITE);
+		
+		setSize(800,480);
+		setLocation(350,200);
+		setVisible(true);
+	}
+
+
+	public static void main(String[] args) {
+		new Login();
+	}
+
+}
